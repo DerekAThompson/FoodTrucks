d90efe596510676b043837af854afcf3f288bf2b
Derek Thompson <derekt123@outlook.com>
2021-08-09 21:33:53 -0500

    Delete Import_Modify.py

--- Import_Modify.py
+++
@@ -1,8 +0,0 @@
-import pandas
-
-url = 'https://data.sfgov.org/api/views/rqzj-sfat/rows.csv'
-foodtruckdata = pandas.read_csv(url)
-foodtruckdatanohead = foodtruckdata.drop('locationid')
-print(foodtruckdatanohead)
-#print(type(foodtruckdata))
-#print(foodtruckdata.loc[1])
